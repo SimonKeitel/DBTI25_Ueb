cb14d5b38e147dde36d66f50f73ce15ef0739844
SimonKeitel <89344289+SimonKeitel@users.noreply.github.com>
2025-06-11 20:20:02 +0000

    Schemamigrationsfunktion gebaut

--- JDBC/src/main/java/fhwedel/JDBC/JDBC.java
+++ JDBC/src/main/java/fhwedel/JDBC/JDBC.java
@@ -6,7 +6,6 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
-import com.mysql.cj.protocol.Resultset;
 import java.sql.Connection;
 
 public class JDBC {
@@ -17,18 +16,18 @@ public class JDBC {
             Connection con = connectDB();
 
             // Erstellen eines neuen Datensatzes - Aufgabe 2
-            createNewEmployee(con, 417, "Krause", "Hendrik", "it1", "d13", "tkk");
+            //createNewEmployee(con, 417, "Krause", "Hendrik", "it1", "d13", "tkk");
 
             // Ausgeben der Personen-Tabelle - Aufgabe 3
             showEmployees(con);
 
             //Aktualisieren des Gehalts - Aufgabe 4
-            updateSalary(con, "it1", 0.1);  
+            //updateSalary(con, "it1", 0.1);  
 
             //Löschen eines Mitarbeiters - Aufgabe 5
-            deleteEmployee(con, "Tietze", "Lutz");
+            //deleteEmployee(con, "Tietze", "Lutz");
 
-
+            con.close();
         } catch (Exception e) {
             System.out.println("Beim Öffnen der Datenbankverbindung oder einer SQL Operation ist eine Exception aufgetreten: " + "\n" + e);
         }
@@ -128,6 +127,49 @@ public class JDBC {
             statement.executeUpdate();
         } catch (Exception e) {
             System.out.println("Beim Löschen eines Mitarbeiters ist eine SQL Exception aufgetreten: " + "\n" + e);
+        }
+    }
+
+    private static void migrateSceme(Connection con) {
+        try {
+            // 1. Einführung neues Schema
+            String sqlString = "create table krankenversicherung ( kkid INTEGER, kuerzel varchar(3), name varchar(20), constraint Schluessel_Krankenversicherung primnary key (kkid) );";
+            Statement statement = con.createStatement();
+            statement.executeUpdate(sqlString);
+            
+            sqlString = "create table personal_neu ( pnr interger(4), name char(20) not null, vorname char(20), geh_stufe varchar(4), abt_nr char(3), krankenkasse integer(4), constraint Schluessel_Personal primary key (pnr));";
+            statement = con.createStatement();
+            statement.executeUpdate(sqlString);
+            
+            // 2. Lesen der Daten gemäß alten Schema
+            sqlString = "insert into personal_neu (pnr, name, vorname, geh_stufe, abt_nr, krankenkasse) select pnr, name, vorname, geh_stufe, abt_nr, null from personal";
+            statement = con.createStatement();
+            statement.execute(sqlString);
+
+            // 3. Konvertieren, ergänzen & abändern der Daten
+            statement.executeQuery("insert into krankenversicherung values(1, 'aok', 'Allgemeine Ortskrankenkasse')");
+            statement.executeQuery("insert into krankenversicherung values(2, 'bak', 'Betriebskrankenkasse B. Braun Aesculap')");
+            statement.executeQuery("insert into krankenversicherung values(3, 'bek', 'Barmer Ersatzkasse')");
+            statement.executeQuery("insert into krankenversicherung values(4, 'dak', 'Deutsche Angestelltenkrankenkasse')");
+            statement.executeQuery("insert into krankenversicherung values(5, 'tkk', 'Techniker Krankenkasse')");
+            statement.executeQuery("insert into krankenversicherung values(4, 'kkh', 'Kaufmännische Krankenkasse')");
+
+            // 4. Schreiben Daten gemäß neuem Schema
+            sqlString = "update personal_neu pn join personal p on pn.pnr = p.pnr join krankenversicherung kv on p.krankenkasse = kv.kuerzel sez pn.krankenkasse = kv.kkid";
+            statement.executeUpdate(sqlString);
+
+            // 5. Umbenennen der Tabellen
+            statement.executeQuery("alter table personal rename to personal_alt;");
+            statement.executeQuery("alter table personal_new rename to personal;");
+
+            // 6. Löschen der alten Daten
+            statement.executeQuery("delete from personal_alt;");
+
+            // 7. Löschen des alten Schemas
+            statement.execute("drop table personal_alt");
+
+        } catch (Exception e) {
+            System.out.println("Bei der Schemamigration ist ein Fehler aufgetreten: " + "\n" + e);
         }
     }
 }
